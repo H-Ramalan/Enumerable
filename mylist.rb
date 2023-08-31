0fbba13905899607e2dd2a5126eb86ff67942a98
Yonashenok <yonashenok24@gmail.com>
2023-08-31 19:09:03 +0300

    fix: linter  error on myList file

--- mylist.rb
+++ mylist.rb
@@ -1,9 +1,15 @@
 require_relative 'myenumerable'
 
+
+
 class MyList
   include MyEnumerable
 
+
+
   attr_accessor :list
+
+
 
   def initialize(*args)
     @list = args
@@ -16,11 +22,20 @@ class MyList
   end
 end
 
+
+
 list = MyList.new(1, 2, 3, 4)
 
+
+
 list.each { |e| puts e }
-puts list.all? { |e| e < 5 }
-puts list.all? { |e| e > 5 }
-puts list.any? { |e| e == 2 }
-puts list.any? { |e| e == 5 }
-print list.filter { |e| e.even? }
+
+puts(list.all? { |e| e < 5 })
+
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+
+puts(list.any? { |e| e == 5 })
+
+print(list.filter { |e| e&.even? })
