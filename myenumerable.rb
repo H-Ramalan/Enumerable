40afee5c72175a78be7b0db681728a71f63c9686
Yonashenok <yonashenok24@gmail.com>
2023-08-31 19:11:24 +0300

    fix indentaion error

--- myenumerable.rb
+++ myenumerable.rb
@@ -10,22 +10,17 @@ module MyEnumerable
   def any?
     @list.each do |item|
       value = yield item if block_given?
-
       return value if value == true
     end
-
     value
   end
 
   def filter
     selected = []
-
     @list.each do |item|
       value = yield item if block_given?
-
       selected.push(item) if value == true
     end
-
     selected
   end
 end
--- mylist.rb
+++ mylist.rb
@@ -1,16 +1,7 @@
 require_relative 'myenumerable'
-
-
-
 class MyList
   include MyEnumerable
-
-
-
   attr_accessor :list
-
-
-
   def initialize(*args)
     @list = args
   end
@@ -22,20 +13,10 @@ class MyList
   end
 end
 
-
-
 list = MyList.new(1, 2, 3, 4)
-
-
-
 list.each { |e| puts e }
-
 puts(list.all? { |e| e < 5 })
-
 puts(list.all? { |e| e > 5 })
-
 puts(list.any? { |e| e == 2 })
-
 puts(list.any? { |e| e == 5 })
-
 print(list.filter { |e| e&.even? })
